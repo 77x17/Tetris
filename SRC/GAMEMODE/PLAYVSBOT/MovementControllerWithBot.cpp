a7debc1d121e16ff0536546a1b7a713ad29f25d6
HCMUS-HQHuy <huynhquochuy06012005@gmail.com>
2025-03-28 19:19:43 +0700

    [BOT] dup get

--- SRC/GAMEMODE/PLAYVSBOT/MovementControllerWithBot.cpp
+++ SRC/GAMEMODE/PLAYVSBOT/MovementControllerWithBot.cpp
@@ -12,13 +12,6 @@ MovementControllerWithBot::~MovementControllerWithBot(){}
 
 void MovementControllerWithBot::handlePut() {
     int nLinesRemove = monitor->putIntoMap(curBlock->getCurrentBlock());
-
-    collision = false;
-    curBlock->setter(monitor->getNext());
-    monitor->unlockHold();
-    if (curBlock->gameOver())
-        monitor->setGameOver();
-
     if (nLinesRemove == 0) {
         std::random_device rd; std::mt19937 gen(rd());
         dynamic_cast<MonitorForTwoPlayer*>(monitor)->mapReceiveLineFromCompetitor(gen());
@@ -29,8 +22,12 @@ void MovementControllerWithBot::handlePut() {
     if (nLinesRemove > 0) {
         competitor->handleAddLine(nLinesRemove);
     }
+
+    collision = false;
     curBlock->setter(monitor->getNext());
     monitor->unlockHold();
+    if (curBlock->gameOver())
+        monitor->setGameOver();
 }
 
 void MovementControllerWithBot::setCompetitor(Bot* b) {
